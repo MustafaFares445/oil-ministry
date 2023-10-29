95fec5ab598f23da012450c56bba7ae9b11e2875
Mohammad Alasfar <mohammad.alasfar@outlook.com>
2023-10-29 16:22:47 +0300

    Adding required models

---
+++ app/Models/Link.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Link extends Model
+{
+    use HasFactory;
+
+   /**
+      * The attributes that are mass assignable.
+      *
+      * @var array<int, string>
+      */
+     protected $fillable = [
+        'name',
+        'link',
+        'lang'
+     ];
+
+
+}
